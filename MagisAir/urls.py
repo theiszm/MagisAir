f86d595b25bf85de84921fd5af7d09d35b830f70
enzo1013 <enzopamintuan.1013@gmail.com>
2025-11-20 16:31:01 +0800

    Added models for Booking View. Non-functional yet

--- MagisAir/urls.py
+++ MagisAir/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 	path('accounts/', include('django.contrib.auth.urls')),
 	path('passenger/', include('Passenger.urls', namespace='passenger')),
+    path('', include('bookings.urls'))
 ]
