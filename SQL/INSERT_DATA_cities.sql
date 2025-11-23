131d0a261a5ecd235170fadc6f4e3cabae845ab0
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-23 16:20:12 +0800

    Modified sql script for base_fare.

--- SQL/INSERT_DATA_cities.sql
+++ SQL/INSERT_DATA_cities.sql
@@ -10,5 +10,9 @@ INSERT INTO MagisAir_route (origin_id, destination_id)
 VALUES (1, 2);
 
 -- FLIGHT SCHEDULES & COST --
-INSERT INTO MagisAir_flight (flightcode, departuredate, departuretime, arrivaldate, arrivaltime, flightcost, route_id)
-VALUES ('MA 800', '2026-03-10', '14:00:00', '2026-03-10', '17:30:00', 8500, 1);
+INSERT INTO MagisAir_flight (flightcode, departuredate, departuretime, arrivaldate, arrivaltime, base_fare, route_id)
+VALUES
+    -- Flight MA 800 (Manila -> Hong Kong)
+    ('MA 800', '2026-03-10', '14:00:00', '2026-03-10', '17:30:00', 8500, 1),
+    -- Flight MA 801 (Hong Kong -> Manila)
+    ('MA 801', '2026-03-15', '12:00:00', '2026-03-15', '15:30:00', 8300, 2);
