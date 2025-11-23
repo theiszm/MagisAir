9856d13f5dbbeac4aa7ef6cbd03a1aa047d58721
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-23 23:25:27 +0800

    Merge branch 'SQL'

--- SQL/INSERT_DATA_cities.sql
+++ SQL/INSERT_DATA_cities.sql
@@ -11,9 +11,9 @@ VALUES
     (2, 1);   -- Hong Kong -> Manila
 
 -- FLIGHT SCHEDULES & COST --
-INSERT INTO MagisAir_flight (flight_code, departure_date, departure_time, arrival_date, arrival_time, route_id, base_fare)
+INSERT INTO MagisAir_flight (flight_code, departure, arrival, route_id, base_fare)
 VALUES
     -- Flight MA 800 (Manila -> Hong Kong)
-    ('MA 800', '2026-03-10', '14:00:00', '2026-03-10', '17:30:00', 1, 8500),
+    ('MA 800', '2026-03-10 14:00:00', '2026-03-10 17:30:00', 1, 8500),
     -- Flight MA 801 (Hong Kong -> Manila)
-    ('MA 801', '2026-03-15', '12:00:00', '2026-03-15', '15:30:00', 2, 8300);
+    ('MA 801', '2026-03-15 12:00:00', '2026-03-15 15:30:00', 2, 8300);
