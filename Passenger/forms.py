af134da9b5721a7e537a9396be5b35e02a7a43ed
tttrono <theiss.thella.trono@student.ateneo.edu>
2025-11-27 13:01:51 +0800

    Added country widget for phonenumber.

--- Passenger/forms.py
+++ Passenger/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 
 from bootstrap_datepicker_plus.widgets import DatePickerInput
+from phonenumber_field.formfields import PhoneNumberField as PhoneNumberFormField
 
-from .models import *
+from .models import Passenger
+
 
 class SignupForm(UserCreationForm):
     class Meta: 
@@ -13,19 +15,30 @@ class SignupForm(UserCreationForm):
         fields = ['username', 'email']
 
 class PassengerForm(ModelForm):
-    """A form for updating passenger information. """
+    """A form for updating passenger information."""
+
+    phone_number = PhoneNumberFormField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter phone number",
+                "autocomplete": "off",
+                "style": "padding-left: 50px;"
+            }
+        ),
+        required=False,
+    )
+
     class Meta:
         model = Passenger
-        exclude=['user']
+        exclude = ['user']
         widgets = {
             "birthdate": DatePickerInput(
-                options={
-                    "format": "YYYY-MM-DD",
-                },
-                attrs={
-                    "class": "form-control",
-                }
-            )
+                options={"format": "YYYY-MM-DD"},
+                attrs={"class": "form-control"},
+            ),
         }
+
+        
 
         
