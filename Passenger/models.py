55d3bac5677a9c72eb0e1cb939dad84a1784b18d
NoahJVV <152837418+NoahJVV@users.noreply.github.com>
2025-11-29 21:19:04 +0800

    Added 'Other' as an option for Gender
    To fit in line with the data dictionary, added it as an option

--- Passenger/models.py
+++ Passenger/models.py
@@ -29,7 +29,7 @@ class Passenger(models.Model):
     birthdate = models.DateField(null=True, blank=True)
     gender = models.CharField(
         max_length=1, 
-        choices=[('F', 'Female'), ('M', 'Male')], 
+        choices=[('F', 'Female'), ('M', 'Male'), ('O', 'Other')], 
         default='', null=True, blank=True, 
     )
     
